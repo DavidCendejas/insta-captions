29dea06f69593b2b3b2544825ba8fb7e457a8f3e
David Cendejas <daveeed@Davids-MacBook-Pro-45.local>
2023-02-19 22:21:24 -0500

    Added mandarin model with adjusted parameters, transcription writes to a separate .txt file in a new folder, refactored main to be a function that should eventually be exported

--- src/transcription.py
+++ src/transcription.py
@@ -4,11 +4,22 @@ import os
 import wave
 from deepspeech import Model as dsm
 
-model_path = 'language_models/deepspeech-0.9.3-models.pbmm'
-lm_path = 'language_models/deepspeech-0.9.3-models.scorer'
+models = {
+    'english': 'language_models/deepspeech-0.9.3-models.pbmm',
+    'mandarin': 'language_models/deepspeech-0.9.3-models-zh-CN.pbmm'
+}
+
+lm = {
+    'english': 'language_models/deepspeech-0.9.3-models.scorer',
+    'mandarin': 'language_models/deepspeech-0.9.3-models-zh-CN.scorer'
+}
+
+alphabeta = {
+    'english': {'alpha': 0.931289039105002, 'beta': 1.1834137581510284},
+    'mandarin': {'alpha': 0.6940122363709647, 'beta': 4.777924224113021}
+}
+
 beam_width = 100
-lm_alpha = 0.93
-lm_beta = 1.18
 
 def read_from_wav(filename):
     with wave.open(filename, 'rb') as w:
@@ -24,20 +35,31 @@ def wav_to_text(model, filename):
 
 
 
-def main():
-    if len(sys.argv) == 1:
-        print('At least 1 file must be inputed.\n\nUsage: "./transcription.py filename1 filename2 ...')
+# wav_files is a list of .wav files
+def transcribe(wav_files, language='english'):
+    if len(wav_files) == 0:
+        return []
+    if language not in models:
+        return [] #THIS SHOULD RAISE AN EXCEPTION
     else:
-        model = dsm(model_path)
-        model.enableExternalScorer(lm_path)
-        model.setScorerAlphaBeta(lm_alpha, lm_beta)
+        
+        # Eventually when incorporating SpeechRecognition library to be able to detect
+        # the language of a file, the model parameters should be set for each distinct
+        # .wav file each loop. Language parameter would then be refactored out
+
+        model = dsm(models[language])
+        model.enableExternalScorer(lm[language])
+        model.setScorerAlphaBeta(alphabeta[language]['alpha'],
+            alphabeta[language]['beta'])
         model.setBeamWidth(beam_width)
 
-        for audiofile in sys.argv[1:]:
+        for audio in wav_files:
             try:
-                print(wav_to_text(model, audiofile))
+                filename = os.path.splitext(os.path.basename(audio))[0]
+                with open('transcriptions/{}.txt'.format(filename), 'w') as fp:
+                    transcription = wav_to_text(model, audio)
+                    fp.write(transcription)
             except:
                 print('Invalid File')
 
-if __name__ == '__main__':
-    main()
+transcribe(['audio/english_examples/8455-210777-0068.wav'], 'english')
